2addc6f3c11a3577fd7916f95454b6312fcbde38
ABRAHEEM MOHAMMED <abraheemmoe1@gmail.com>
2024-12-16 20:37:10 -0500

    Create Rustrens.java

---
+++ Rustrens.java
@@ -0,0 +1,76 @@
+printf("\t\t\tPizza \nPrice :%d\n",price);
+        System.out.print("Enter How Many DO you Have : ");
+        int order =input.nextInt();
+        Totel = price*order;
+        System.out.printf("The Totel :%d\n",Totel);
+        System.out.print("The order is in process. Wait five minutes, please\n\n");    
+    } 
+     //============== (2) ===========
+     public void Cheese_pizza()
+    {
+        Scanner input= new Scanner(System.in);
+        int price =13;int Totel;
+        System.out.printf("\t\t\tCheese pizza \nPrice :%d\n",price);
+        System.out.print("Enter How Many DO you Have : ");
+        int order =input.nextInt();
+        Totel = price*order;
+        System.out.printf("The Totel :%d\n",Totel);
+        System.out.print("The order is in process. Wait five minutes, please\n\n");    
+    } 
+    //===================================================
+     
+    //================== Meals ====================
+     //============ (1) =========
+     public void Kebab_meal()
+    {
+        Scanner input= new Scanner(System.in);
+        int price =18;int Totel;
+        System.out.printf("\t\t\tKebab meal \nPrice :%d\n",price);
+        System.out.print("Enter How Many DO you Have : ");
+        int order =input.nextInt();
+        Totel = price*order;
+        System.out.printf("The Totel :%d\n",Totel);
+        System.out.print("The order is in process. Wait five minutes, please\n\n");    
+    } 
+    //=========== (2) ==========
+      public void chicken_meal()
+    {
+        Scanner input= new Scanner(System.in);
+        int price =20;int Totel;
+        System.out.printf("\t\t\tchicken meal \nPrice :%d\n",price);
+        System.out.print("Enter How Many DO you Have : ");
+        int order =input.nextInt();
+        Totel = price*order;
+        System.out.printf("The Totel :%d\n",Totel);
+        System.out.print("The order is in process. Wait five minutes, please\n\n");    
+    } 
+    //============ (3) ==========
+      public void meat_meal()
+    {
+        Scanner input= new Scanner(System.in);
+        int price =25;int Totel;
+        System.out.printf("\t\t\tmeat meal meal \nPrice :%d\n",price);
+        System.out.print("Enter How Many DO you Have : ");
+        int order =input.nextInt();
+        Totel = price*order;
+        System.out.printf("The Totel :%d\n",Totel);
+        System.out.print("The order is in process. Wait five minutes, please\n\n");    
+    } 
+      
+      public void conaction()
+      {
+            System.out.print("\n\n\n------------ Conaction us -----------\n");
+            System.out.println(phone);
+            System.out.println(Email);
+            System.out.println(FAcount);
+            System.out.println(Adrees);
+            System.out.print("------------ THNCK YOU ---------------\n\n");
+      }
+      public void Exit()
+      {
+            System.out.print("\n\n\n--------------------------------------\n");
+            System.out.println("        Thank For Use Our Servses      ");
+            System.out.print("\n--------------------------------------\n");
+      }
+}
+//===================================  THANK YOU   ============================================
